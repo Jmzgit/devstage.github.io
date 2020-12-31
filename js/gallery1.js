3a03d62e619e815e532796fa8f7d75b503d4a270
son2o3 <aureliojmz@gmail.com>
2020-12-30 22:40:29 -0800

    Finally done transition to sample - tweaked JS onmouseout function in JS file

--- js/gallery1.js
+++ js/gallery1.js
@@ -1,13 +1,12 @@
-document.getElementById('smallBlock')
+document.getElementById('smallBlock');
 
 function upDate(previewPic){
 document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
-
-
-document.getElementById('id').setAttribute("id", "smallBlock"); 
-
+document.getElementById('smallBlock').style.transition = "all 1.2s";
 }
 
 function unDo(){
+   document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
+   document.getElementById('smallBlock').style.transition = "all 1.2s";
    //do nothing
 }

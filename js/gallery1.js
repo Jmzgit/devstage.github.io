506dd918ed73e62c8c458366d7b04c68ce09a172
son2o3 <aureliojmz@gmail.com>
2020-12-30 21:41:54 -0800

    Transicion

--- js/gallery1.js
+++ js/gallery1.js
@@ -1,10 +1,9 @@
 document.getElementById('smallBlock')
+
 function upDate(previewPic){
 document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
 }
 
 function unDo(){
-
-document.getElementById('smallBlock').style.background='#fff';
-
+   //do nothing
 }

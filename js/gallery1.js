e71fbe0245a00876f61723905a00fc77ac48806e
son2o3 <aureliojmz@gmail.com>
2020-12-31 00:37:11 -0800

    email - need to do flex solution

--- js/gallery1.js
+++ js/gallery1.js
@@ -2,11 +2,11 @@ document.getElementById('smallBlock');
 
 function upDate(previewPic){
 document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
-document.getElementById('smallBlock').style.transition = "all 1.2s";
+document.getElementById('smallBlock').style.transition = "all 1.6s";
 }
 
 function unDo(){
    document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
-   document.getElementById('smallBlock').style.transition = "all 1.2s";
+   document.getElementById('smallBlock').style.transition = "all 2.3s";
    //do nothing
 }

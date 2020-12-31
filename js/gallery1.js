16532f85b03f241a48400752b98a38c3a97ddc75
Aurelio <aureliojmz@gmail.com>
2020-12-30 19:55:47 -0800

    Transition on background colors

---
+++ js/gallery1.js
@@ -0,0 +1,10 @@
+document.getElementById('smallBlock')
+function upDate(previewPic){
+document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
+}
+
+function unDo(){
+
+document.getElementById('smallBlock').style.background='#fff';
+
+}

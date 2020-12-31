ba9a6d891b8bd7993df90f083c9c9094994adb91
son2o3 <aureliojmz@gmail.com>
2020-12-30 22:05:47 -0800

    Nice transitions - added image to same CSS attribute that had transition+opacity

--- js/gallery1.js
+++ js/gallery1.js
@@ -2,6 +2,10 @@ document.getElementById('smallBlock')
 
 function upDate(previewPic){
 document.getElementById('smallBlock').style.background="url(' "+previewPic.src+" ')";
+
+
+document.getElementById('id').setAttribute("id", "smallBlock"); 
+
 }
 
 function unDo(){

f71ed908b84d3234bf522e3dfb051bf04a96a728
Aurelio <aureliojmz@gmail.com>
2021-01-04 15:15:10 -0800

    Transition not to happy

--- js/gallery1.js
+++ js/gallery1.js
@@ -1,1 +1,10 @@
-document.getElementById("smallBlock");function upDate(a){document.getElementById("smallBlock").style.background="url(' "+a.src+" ')";document.getElementById("smallBlock").style.transition="all 1.6s"}function unDo(a){document.getElementById("smallBlock").style.background="url(' "+a.src+" ')";document.getElementById("smallBlock").style.transition="all 2.3s"};
+document.getElementById("smallBlock");
+
+function upDate(a){
+
+   document.getElementById("smallBlock").style.background="url(' "+a.src+" ')";
+   document.getElementById("smallBlock").style.transition="all 1.6s"}
+
+function unDo(a){
+   document.getElementById("smallBlock").style.background="url(' "+a.src+" ')";
+   document.getElementById("smallBlock").style.transition="all 2.3s"};

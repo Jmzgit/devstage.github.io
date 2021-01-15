d61455f1234e60cbc2fe4a1ae78e19b8235e9599
son2o3 <aureliojmz@gmail.com>
2021-01-15 14:53:11 -0800

    took out minify of js

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,1 +1,10 @@
-var myPix=["img/cool fruit juice.webp","img/Indienne Blue.webp","img/nice orange yellow.webp","img/Ocean Pastels.webp","img/OrangesScale.webp","img/Princess Pink.webp","img/Yellow the green.webp","img/Violet Violence.webp"];setInterval(function walkPix(){var a=myPix.length;var b=Math.floor(Math.random()*a-0);document.getElementById("smallBlock").src=myPix[b]},5000);function myEmail(){location.href="mailto:info@weknowstudio.com"};
+var myPix = ["img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Ocean Pastels.webp", "img/OrangesScale.webp", "img/Princess Pink.webp", "img/Yellow the green.webp", "img/Violet Violence.webp"];
+setInterval(function walkPix() {
+   var a = myPix.length;
+   var b = Math.floor(Math.random() * a - 0);
+   document.getElementById("smallBlock").src = myPix[b]
+}, 5000);
+
+function myEmail() {
+   location.href = "mailto:info@weknowstudio.com"
+};

1d3f831456526e1c07463969c1d4614a9f253481
son2o3 <aureliojmz@gmail.com>
2021-01-14 21:29:01 -0800

    deleted palettes colors that are shit

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,5 +1,5 @@
 
-var myPix = ["img/OrangesScale Color Palette - color-hex.com.webp", "img/Piggy greens Color Palette - color-hex.com.webp", "img/Ocean Pastels Color Palette - color-hex.com.webp", "img/Violet Violence Color Palette - color-hex.com.webp", "img/nice orange yellow mix Color Palette - color-hex.com.webp", "img/--cool fruit juice-- Color Palette - color-hex.com.webp", "img/OrangesScale Color Palette - color-hex.com.webp", "img/Princess Pink Color Palette - color-hex.com.webp", "img/S u n n y Color Palette - color-hex.com.webp", "img/Indienne Blues Color Palette - color-hex.com.webp"];
+var myPix = ["img/OrangesScale Color Palette - color-hex.com.webp", "img/Ocean Pastels Color Palette - color-hex.com.webp", "img/Violet Violence Color Palette - color-hex.com.webp", "img/nice orange yellow mix Color Palette - color-hex.com.webp", "img/--cool fruit juice-- Color Palette - color-hex.com.webp", "img/OrangesScale Color Palette - color-hex.com.webp", "img/Princess Pink Color Palette - color-hex.com.webp", "img/Indienne Blues Color Palette - color-hex.com.webp"];
 setInterval(function walkPix() {
    var size = myPix.length;
    var randomNum = Math.floor(Math.random() * size);

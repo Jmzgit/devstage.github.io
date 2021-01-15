548570a5ecf78391571089cb178a5df37ead18a2
son2o3 <aureliojmz@gmail.com>
2021-01-15 14:41:12 -0800

    same

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,1 +1,11 @@
-var myPix=["img/cool fruit juice.webp","img/Indienne Blue.webp","img/nice orange yellow.webp","img/Ocean Pastels.webp","img/OrangesScale.webp","img/Princess Pink.webp","img/Yellow the green.webp","img/Violet Violence.webp"];setInterval(function walkPix(){var d=myPix.length;var c=Math.floor(Math.random()*d-0);document.getElementById("smallBlock").src=myPix[c];document.getElementById("MyAlt").innerHTML=(smallBlock.src+c)},5000);function myEmail(){location.href="mailto:info@weknowstudio.com"};
+var myPix = ["img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Ocean Pastels.webp", "img/OrangesScale.webp", "img/Princess Pink.webp", "img/Yellow the green.webp", "img/Violet Violence.webp"];
+setInterval(function walkPix() {
+   var d = myPix.length;
+   var c = Math.floor(Math.random() * d - 0);
+   document.getElementById("smallBlock").src = myPix[c];
+   document.getElementById("MyAlt").innerHTML = (smallBlock.src + c)
+}, 5000);
+
+function myEmail() {
+   location.href = "mailto:info@weknowstudio.com"
+};

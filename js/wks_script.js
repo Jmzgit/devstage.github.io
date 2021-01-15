e7e6d22cccf77f4cb0bea4edc983dcccdd9ccdd0
Aurelio <aureliojmz@gmail.com>
2021-01-14 17:13:34 -0800

    Formatting and added external js file to handel js

---
+++ js/wks_script.js
@@ -0,0 +1,21 @@
+var myPix = [
+   "img/OrangesScale Color Palette - color-hex.com.webp",
+   "img/Piggy greens Color Palette - color-hex.com.webp",
+   "img/Ocean Pastels Color Palette - color-hex.com.webp",
+   "img/Nowaday01 Color Palette - color-hex.com.webp",
+   "img/nice orange yellow mix Color Palette - color-hex.com.webp",
+   "img/--cool fruit juice-- Color Palette - color-hex.com.webp",
+   "img/OrangesScale Color Palette - color-hex.com.webp",
+   "img/Day to Night Color Palette - color-hex.com.webp",
+   "img/S u n n y Color Palette - color-hex.com.webp",
+   "img/Indienne Blues Color Palette - color-hex.com.webp"
+];
+setInterval(function walkPix() {
+   var size = myPix.length;
+   var randomNum = Math.floor(Math.random() * size);
+   document.getElementById("smallBlock").src = myPix[randomNum];
+}, 5000);
+
+function myEmail(){
+location.href="mailto:mail@weknowstudio.com";
+}

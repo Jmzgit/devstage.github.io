d59420534ccd8147fc6757c7799fe4655c40458d
son2o3 <aureliojmz@gmail.com>
2021-01-15 12:43:35 -0800

    Image editting and shorting names

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,5 +1,13 @@
 
-var myPix = ["img/OrangesScale Color Palette - color-hex.com.webp", "img/Ocean Pastels Color Palette - color-hex.com.webp", "img/Violet Violence Color Palette - color-hex.com.webp", "img/nice orange yellow mix Color Palette - color-hex.com.webp", "img/--cool fruit juice-- Color Palette - color-hex.com.webp", "img/OrangesScale Color Palette - color-hex.com.webp", "img/Princess Pink Color Palette - color-hex.com.webp", "img/Indienne Blues Color Palette - color-hex.com.webp"];
+var myPix = [
+"img/cool fruit juice.webp",
+"img/Indienne Blue.webp",
+"img/nice orange yellow.webp",
+"img/Ocean Pastels.webp",
+"img/OrangesScale.webp",
+"img/Princess Pink.webp",
+"img/Yellow the green.webp",
+"img/Violet Violence.webp"];
 setInterval(function walkPix() {
    var size = myPix.length;
    var randomNum = Math.floor(Math.random() * size);

011acfe966e34f6e363b399ca53617b2c9f23669
son2o3 <aureliojmz@gmail.com>
2021-01-15 17:28:00 -0800

    yes

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,10 +1,1 @@
-var myPix = ["img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Ocean Pastels.webp", "img/OrangesScale.webp", "img/Princess Pink.webp", "img/Violet Violence.webp"];
-setInterval(function walkPix() {
-   var a = myPix.length;
-   var b = Math.floor(Math.random() * a - 0);
-   document.getElementById("smallBlock").src = myPix[b]
-}, 5000);
-
-function myEmail() {
-   location.href = "mailto:info@weknowstudio.com"
-};
+var myPix=["img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp","img/pinky promised to keep it platonic.webp","img/cool fruit juice.webp","img/Indienne Blue.webp","img/nice orange yellow.webp","img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp"];setInterval(function walkPix(){var d=myPix.length;var c=Math.floor(Math.random()*d-0);document.getElementById("smallBlock").src=myPix[c]},5000);function myEmail(){location.href="mailto:info@weknowstudio.com"};

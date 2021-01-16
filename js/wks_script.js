6990e502a6e3b4915815f342edd706d7879d6a62
son2o3 <aureliojmz@gmail.com>
2021-01-15 16:26:40 -0800

    Deleting Green Bright yellow palette

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,4 +1,4 @@
-var myPix = ["img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Ocean Pastels.webp", "img/OrangesScale.webp", "img/Princess Pink.webp", "img/Yellow the green.webp", "img/Violet Violence.webp"];
+var myPix = ["img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Ocean Pastels.webp", "img/OrangesScale.webp", "img/Princess Pink.webp", "img/Violet Violence.webp"];
 setInterval(function walkPix() {
    var a = myPix.length;
    var b = Math.floor(Math.random() * a - 0);

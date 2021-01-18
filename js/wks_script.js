691d046bb266015dd3ab05e30f605f25a597164f
son2o3 <aureliojmz@gmail.com>
2021-01-17 21:40:47 -0800

    Phone instead of email in footer link

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,1 +1,10 @@
-var myPix=["img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp","img/pinky promised to keep it platonic.webp","img/cool fruit juice.webp","img/Indienne Blue.webp","img/nice orange yellow.webp","img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp"];setInterval(function walkPix(){const d=myPix.length;const c=Math.floor(Math.random()*d-0);document.getElementById("smallBlock").src=myPix[c]},5000);function myEmail(){location.href="mailto:info@weknowstudio.com"};
+var myPix = ["img/Pretty Perfect Her Pink.webp", "img/Pinky winona.webp", "img/Princess Pink.webp", "img/Violet Violence.webp", "img/pinky promised to keep it platonic.webp", "img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Pretty Perfect Her Pink.webp", "img/Pinky winona.webp", "img/Princess Pink.webp", "img/Violet Violence.webp"];
+setInterval(function walkPix() {
+   const d = myPix.length;
+   const c = Math.floor(Math.random() * d - 0);
+   document.getElementById("smallBlock").src = myPix[c]
+}, 5000);
+
+function myMobile() {
+   location.href = "tel:+526462408973"
+};

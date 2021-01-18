62c1c020ecfd9dab2540d3d7c0bd45924db55550
son2o3 <aureliojmz@gmail.com>
2021-01-17 23:25:48 -0800

    yes

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,10 +1,1 @@
-var myPix = ["img/Pretty Perfect Her Pink.webp", "img/Pinky winona.webp", "img/Princess Pink.webp", "img/Violet Violence.webp", "img/pinky promised to keep it platonic.webp", "img/cool fruit juice.webp", "img/Indienne Blue.webp", "img/nice orange yellow.webp", "img/Pretty Perfect Her Pink.webp", "img/Pinky winona.webp", "img/Princess Pink.webp", "img/Violet Violence.webp"];
-setInterval(function walkPix() {
-   const d = myPix.length;
-   const c = Math.floor(Math.random() * d - 0);
-   document.getElementById("smallBlock").src = myPix[c]
-}, 5000);
-
-function myMobile() {
-   href = "tel:+526462408973";
-};
+var myPix=["img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp","img/pinky promised to keep it platonic.webp","img/cool fruit juice.webp","img/Indienne Blue.webp","img/nice orange yellow.webp","img/Pretty Perfect Her Pink.webp","img/Pinky winona.webp","img/Princess Pink.webp","img/Violet Violence.webp"];setInterval(function walkPix(){const a=myPix.length;const b=Math.floor(Math.random()*a-0);document.getElementById("smallBlock").src=myPix[b]},5000);function myMobile(){href="tel:+526462408973"};
